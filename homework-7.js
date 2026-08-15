dbc76591edc835e1f1a4dbd4435b7602ecb7ee0e
MuhammadDzhalilov <myitschool2026@gmail.com>
2026-08-15 22:58:24 +0300

    feat: init homework-7.js

---
+++ homework-7.js
@@ -0,0 +1,36 @@
+function calcTemperature(city, value) {
+  console.log(`Сейчас в городе ${city} температура - ${value} градусов по Цельсию`)
+}
+
+calcTemperature('Хасавюрт', 19)
+
+const LIGHT_SPEED = 299792458;
+
+function checkSpeed(speed) {
+  if (speed > LIGHT_SPEED) {
+    console.log('Сверхсветовая скорость')
+  } else if (speed === LIGHT_SPEED) {
+    console.log('Скорость света')
+  } else {
+    console.log('Субсветовая скорость')
+  }
+}
+
+checkSpeed(36)
+checkSpeed(299792458)
+checkSpeed(399792458)
+
+const productPrice = 1337
+const productName = "Яблоки у куринном бульоне"
+
+function buyProduct(budget) {
+  if (budget >= productPrice) {
+    console.log(`Товар "${productName}" приобретён. Спасибо за покупку!`)
+  } else {
+    const difference = productPrice - budget
+    console.log(`Вам не хватает ${difference}$, пополните баланс`)
+  }
+}
+
+buyProduct(1300)
+buyProduct(1500)
